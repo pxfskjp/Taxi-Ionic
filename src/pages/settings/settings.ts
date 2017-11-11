43f4083201f68173cc42192ea9d6aed16b68ecb2
topdeveloper0729 <topdev0729@gmail.com>
2017-11-11 17:15:08 +0200

    updated settings message

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NavController, NavParams, ModalController} from 'ionic-angular';
 import {AppVersion} from '@ionic-native/app-version';
 import {ConfigService} from '../../providers/config-service/config-service';
+import {ConfigModel} from '../../models/config/config.model';
 import {LanguagePage} from '../language/language';
 import {TranslateService} from '@ngx-translate/core';
 
@@ -15,6 +16,7 @@ export class SettingsPage {
   public packageName: any;
   public versionCode: any;
   public versionNumber: any;
+  public areasCount: number;
 
   constructor(
     public navCtrl: NavController,
@@ -30,6 +32,10 @@ export class SettingsPage {
     this.appVersion.getPackageName().then(data => this.packageName = data);
     this.appVersion.getVersionCode().then(data => this.versionCode = data);
     this.appVersion.getVersionNumber().then(data => this.versionNumber = data);
+    
+    this.configService.getAll().then((configModel: ConfigModel) => {
+      this.areasCount = configModel.areas.length; 
+    })
   }
 
   changeLanguage() {
